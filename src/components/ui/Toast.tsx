a26541a7d03cccfc8b616ea01b9aac271a277195
eneabugrac <enesbc46@gmail.com>
2025-04-19 13:22:08 +0300

    style: change toast position

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -39,61 +39,9 @@ export const Toast: React.FC<ToastProps> = ({
     }
   };
 
-  const getIcon = () => {
-    switch (type) {
-      case "success":
-        return (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-              clipRule="evenodd"
-            />
-          </svg>
-        );
-      case "error":
-        return (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-              clipRule="evenodd"
-            />
-          </svg>
-        );
-      case "info":
-        return (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
     <div
-      className={`fixed bottom-6 right-6 z-50 transform transition-all duration-300 ${
+      className={`fixed top-6 right-6 z-50 transform transition-all duration-300 ${
         visible
           ? "translate-y-0 opacity-100"
           : "translate-y-8 opacity-0 pointer-events-none"
@@ -102,14 +50,13 @@ export const Toast: React.FC<ToastProps> = ({
       <div
         className={`flex items-center backdrop-blur-sm border rounded-lg shadow-lg max-w-sm p-4 ${getTypeStyles()}`}
       >
-        <div className="flex-shrink-0 mr-3 text-white">{getIcon()}</div>
         <div className="text-white font-medium">{message}</div>
         <button
           onClick={() => {
             setVisible(false);
             setTimeout(onClose, 300);
           }}
-          className="ml-4 text-white opacity-80 hover:opacity-100 transition-opacity"
+          className="ml-4 text-white cursor-pointer opacity-80 hover:opacity-100 transition-opacity"
         >
           X
         </button>
